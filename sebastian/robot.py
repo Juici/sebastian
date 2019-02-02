1395c2c174083a1226aceeab1ff8edb4e903ccdc
James Whaley <juicy66173@gmail.com>
2019-02-02 21:43:54 +0000

    Add initial GPIO logic

--- sebastian/robot.py
+++ sebastian/robot.py
@@ -1,10 +1,32 @@
+from .rpi import RPi
+
+# Angle per turn command.
+TURN_ANGLE = 10
+
+
 class Robot:
     """
     Sebastian the robot.
     """
 
     def __init__(self):
-        pass
+        # TODO: initialize twitch stuff
+        self.rpi = RPi()
 
     def run(self):
-        pass
+        # TODO: start twitch hooks
+
+        self.on_command('left')
+
+        import time
+        time.sleep(5)
+
+    def on_command(self, cmd):
+        switcher = {
+            'forward': self.rpi.forward,
+            'left': self.rpi.left,
+            'right': self.rpi.right,
+            'stop': self.rpi.stop
+        }
+        cmd = str(cmd).lower()
+        switcher[cmd]()
---
+++ sebastian/rpi.py
@@ -0,0 +1,49 @@
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    raise RuntimeError('Error importing RPi.GPIO. Superuser privileges may be required.')
+
+# LEFT_WHEEL = 12
+# RIGHT_WHEEL = 13
+
+LEFT_WHEEL = 32
+RIGHT_WHEEL = 33
+
+CHANNELS = [LEFT_WHEEL, RIGHT_WHEEL]
+
+FREQ = 50
+DC = 7.5
+
+
+class RPi:
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+
+        GPIO.setup(CHANNELS, GPIO.OUT)
+
+        self._left = GPIO.PWM(LEFT_WHEEL, FREQ)
+        self._right = GPIO.PWM(RIGHT_WHEEL, FREQ)
+
+    def forward(self):
+        self._left.start(DC)
+        self._right.start(DC)
+
+    def left(self):
+        self._left.start(DC)
+        self._right.stop()
+
+    def right(self):
+        self._right.start(DC)
+        self._left.stop()
+
+    def stop(self):
+        self._left.stop()
+        self._right.stop()
+
+    def __del__(self):
+        self._left.stop()
+        self._right.stop()
+
+        GPIO.cleanup(CHANNELS)
